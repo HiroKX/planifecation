4e67f0f4acea62ef18292028d5be303cb8dcd2c2
robin <57806687+HiroKX@users.noreply.github.com>
2023-11-13 16:02:50 +0100

    Ended PoC

--- Application/src/components/GoogleUp.tsx
+++ Application/src/components/GoogleUp.tsx
@@ -3,14 +3,16 @@ import * as React from "react";
 import * as WebBrowser from "expo-web-browser";
 import * as Linking from "expo-linking";
 import { useNavigation } from '@react-navigation/native';
+import {useState} from "react";
 
 function GoogleUp() {
     const navigation = useNavigation();
-    const YOUR_CLIENT_ID = ""
+    const YOUR_CLIENT_ID = "273744401902-ukvoh9f1m229ok383rcvrclnlnatno9d.apps.googleusercontent.com"
     const YOUR_REDIRECT_URI = "http://localhost:3001/google"
     const REDIRECT_URI = "exp://192.168.1.59:8081"
-    const handlePress = async () => {
+    const [user, setUser] = useState({});
 
+    const handlePress = async () => {
         const result = await WebBrowser.openAuthSessionAsync(
             `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${YOUR_CLIENT_ID}&redirect_uri=${YOUR_REDIRECT_URI}&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile&access_type=offline&prompt=consent`,
             REDIRECT_URI
@@ -20,16 +22,16 @@ function GoogleUp() {
 
             // get back the params from the url
             const params = Linking.parse(result.url);
-
             const { email, name, picture } = params.queryParams;
-
             //pass in all the user data in an object...
-            const user = {
+            const currentUser = {
                 email,
                 name,
                 picture,
             };
-            navigation.navigate('Home', { user });
+            setUser(currentUser)
+
+            navigation.navigate('Home');
             // navigate to the HomeScreen and pass the user object
 
         }
--- Application/src/components/HomeScreen.tsx
+++ Application/src/components/HomeScreen.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import GoogleUp from './GoogleUp';
-function HomeScreen({ route }) {
-    const { user } = route.params;
-    console.log(user);
-    const [userInfo, setUserInfo] = React.useState(null);
-    const [email, setEmail] = React.useState(null);
-    const [name, setName] = React.useState(null);
-    const [picture, setPicture] = React.useState(null);
+function HomeScreen() {
+    //const { user } = route.params;
+    //console.log(user);
+    const [user, setUser] = React.useState({
+        email :"null",
+        name :"null",
+        picture :"null"
+    });
 
 
 // then display the data!
     return (
         <View style={styles.container}>
             {/* print props */}
-            {<Image source={{uri: picture}} style={{width: 100, height: 100}}/>}
+            {/*<Image source={{uri: user.picture}} style={{width: 100, height: 100}}/>*/}
             <Text style={styles.info}>Name: {user.name}</Text>
             <Text style={styles.info}>Email: {user.email}</Text>
             <GoogleUp />
