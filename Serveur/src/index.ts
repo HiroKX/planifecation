39b97986bd875406ef94aff4acce6c0725bbfaa8
Thomas Felker <thomas.felker8@etu.univ-lorraine.fr>
2023-11-14 15:39:51 +0100

    Ajout du nouveau serveur ApolloGraphQL

---
+++ Serveur/src/index.ts
@@ -0,0 +1,48 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "User" type defines the queryable fields for every user in our data source.
+  type User {
+    id: String
+    login: String
+    password: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "users" query returns an array of zero or more Users (defined above).
+  type Query {
+    users: [User]
+  }
+`;
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves users from the "users" array above.
+const resolvers = {
+    Query: {
+        users: () => [ {id: '1', login: 'test', password: 'test'}],
+    },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
