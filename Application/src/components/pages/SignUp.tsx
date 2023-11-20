228f27a2b6ea6b59a3fa68cb03435a858d577834
unknown <thomas.felker8@etu.univ-lorraine.fr>
2023-11-20 14:27:54 +0100

    Ajout de la création de compte
    Ajout de la création de compte via l'API GraphQL lors d'un clic sur le bouton "M'inscrire"

--- Application/src/components/pages/SignUp.tsx
+++ Application/src/components/pages/SignUp.tsx
@@ -1,46 +1,47 @@
-import { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import {useState} from 'react';
 import SurfaceTemplate from '../templates/SurfaceTemplate';
 import TextInputTemplate from '../templates/TextInputTemplate';
 import ButtonTemplate from '../templates/ButtonTemplate';
+import {ApolloConsumer} from "@apollo/client";
+import SignUpService from "../../services/SignUpService";
 
-export default function SignUp({ navigation }) {
-    const [id, setId] = useState("");
+export default function SignUp({ navigation }: any) {
+    const [login, setLogin] = useState("");
     const [password, setPassword] = useState("");
-    const [confirmpass, setConfirmpass] = useState("");
-  return (
-    <SurfaceTemplate>
-      <TextInputTemplate 
-        label="Adresse mail"
-        value={id}
-        handleChangeText={(text) => setId(text)}
-        />
-      <TextInputTemplate
-        label="Mot de passe"
-        mode='outlined'
-        value={password}
-        handleChangeText={(text) => setPassword(text)}
-        secureTextEntry={true}/>
-      <TextInputTemplate
-        label="Confirmer le mot de passe"
-        mode='outlined'
-        value={confirmpass}
-        handleChangeText={(text) => setConfirmpass(text)}
-        secureTextEntry={true}/>
-        <ButtonTemplate
-        handleClick={() => {
-          navigation.replace('Connexion');
-        }}>
-          M'inscrire</ButtonTemplate>
-    </SurfaceTemplate>
-  );
-}
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ccc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return (
+        <ApolloConsumer>
+            { client =>
+                <SurfaceTemplate>
+                    <TextInputTemplate
+                      label="Adresse mail"
+                      value={login}
+                      handleChangeText={(text) => setLogin(text)}
+                    />
+                    <TextInputTemplate
+                      label="Mot de passe"
+                      mode='outlined'
+                      value={password}
+                      handleChangeText={(text) => setPassword(text)}
+                      secureTextEntry={true}
+                    />
+                    <TextInputTemplate
+                      label="Confirmer le mot de passe"
+                      mode='outlined'
+                      value={confirmPassword}
+                      handleChangeText={(text) => setConfirmPassword(text)}
+                      secureTextEntry={true}
+                    />
+                    <ButtonTemplate
+                      handleClick={() => {
+                        SignUpService({client, login, password});
+                        navigation.replace('Connexion');
+                      }}>
+                      M'inscrire
+                    </ButtonTemplate>
+                </SurfaceTemplate>
+            }
+        </ApolloConsumer>
+    );
+}
