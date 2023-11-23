689b4c71ea0f4c8641b621c037e79ab5bc1d1048
Tim <tim.branstett@gmail.com>
2023-11-23 22:33:07 +0100

    Prise en compte de tous params pour PaperButton

--- Application/src/components/templates/ButtonTemplate.tsx
+++ Application/src/components/templates/ButtonTemplate.tsx
@@ -1,11 +1,31 @@
 import { Button as PaperButton } from 'react-native-paper';
-import { GestureResponderEvent, StyleSheet } from 'react-native';
+import { IconSource } from 'react-native-paper/lib/typescript/components/Icon';
+import { Animated, ColorValue, GestureResponderEvent, StyleProp, StyleSheet, TextStyle, ViewStyle } from 'react-native';
 
 interface ButtonOptions {
-  children: React.ReactNode;
   mode?: 'contained' | 'outlined' | 'text' | 'elevated' | 'contained-tonal';
+  dark? : boolean;
+  compact?: boolean;
+  buttonColor?: string;
   textColor?: string;
+  rippleColor?: ColorValue;
+  loading?: boolean;
+  icon?: IconSource;
+  disabled?: boolean;
+  children?: React.ReactNode;
+  uppercase?: boolean;
+  accessibilityLabel?: string;
+  accessibilityHint?: string;
   onPress?: (e : GestureResponderEvent) => void;
+  onPressIn?: (e: GestureResponderEvent) => void;
+  onPressOut?: (e: GestureResponderEvent) => void;
+  onLongPress?: (e: GestureResponderEvent) => void;
+  delayLongPress?: number;
+  contentStyle?: StyleProp<ViewStyle>;
+  maxFontSizeMultiplier?: number;
+  style?: Animated.WithAnimatedValue<StyleProp<ViewStyle>>;
+  labelStyle?: StyleProp<TextStyle>;
+  testID?: string;
 }
 
 export default function ButtonTemplate(props: Readonly<ButtonOptions>) {
@@ -13,8 +33,26 @@ export default function ButtonTemplate(props: Readonly<ButtonOptions>) {
         <PaperButton
         style={styles.button}
         mode={props.mode ?? 'contained'}
+        dark= {props.dark}
+        compact= {props.compact}
+        buttonColor= {props.buttonColor}
         textColor={props.textColor}
+        rippleColor={props.rippleColor}
+        loading={props.loading}
+        icon={props.icon}
+        disabled={props.disabled}
+        uppercase={props.uppercase}
+        accessibilityLabel={props.accessibilityLabel}
+        accessibilityHint= {props.accessibilityHint}
         onPress={props.onPress}
+        onPressIn={props.onPressIn}
+        onPressOut={props.onPressOut}
+        onLongPress={props.onLongPress}
+        delayLongPress={props.delayLongPress}
+        contentStyle={props.contentStyle}
+        maxFontSizeMultiplier={props.maxFontSizeMultiplier}
+        labelStyle= {props.labelStyle}
+        testID={props.testID}
         >
           {props.children}
       </PaperButton>
