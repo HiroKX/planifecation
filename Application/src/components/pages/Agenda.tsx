4e9869e37985e012a637ea27e2f2d690e2d6861c
Tim <tim.branstett@gmail.com>
2023-12-07 23:33:55 +0100

    Implémentations Tabs + Test#1 Calendar
    Implémentation générale de Tabs pour être utilisée plus tard à d'autres endroits
    
    Test de react-native-calendars, module insatisfaisant car pas très extensible (thème dynamique impossible, et re-render tout le calendrier au changement de thème est lourd et pas envisageable)

--- Application/src/components/pages/Agenda.tsx
+++ Application/src/components/pages/Agenda.tsx
@@ -1,9 +1,32 @@
-import SurfaceTemplate from '../organisms/SurfaceTemplate';
+import { View } from 'react-native';
+import TabScreenTemplate from '../molecules/TabScreenTemplate';
+import TabsTemplate from '../organisms/TabsTemplate';
+import CalendarTemplate from '../molecules/CalendarTemplate';
+import { useState } from 'react';
 
-export default function Dashboard() {
+export default function Agenda() {
+
+    const [selected, setSelected] = useState('');
+    
     return (
-        <SurfaceTemplate>
-
-        </SurfaceTemplate>
+        <TabsTemplate>
+            <TabScreenTemplate label='Mois' icon='calendar'>
+                <View>
+                <CalendarTemplate
+                onDayPress={day => {
+                    setSelected(day.dateString);
+                  }}
+                markedDates={{
+                    [selected]: {selected: true, disableTouchEvent: true, selectedColor: 'orange'}
+                  }}/>
+                </View>
+            </TabScreenTemplate>
+            <TabScreenTemplate label='Jour' icon='view-day'>
+                <View></View>
+            </TabScreenTemplate>
+            <TabScreenTemplate label='Details' icon='account-details' disabled={true}>
+            <View></View>
+                </TabScreenTemplate>
+        </TabsTemplate>
     );
-}
+  }
