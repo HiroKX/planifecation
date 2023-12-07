5b8e8a7f8ea1abde6f26ddd6ddc1518c86c299de
Tim <tim.branstett@gmail.com>
2023-12-07 23:33:55 +0100

    Implémentations Tabs + Test#1 Calendar
    Implémentation générale de Tabs pour être utilisée plus tard à d'autres endroits
    
    Test de react-native-calendars, module insatisfaisant car pas très extensible (thème dynamique impossible, et re-render tout le calendrier au changement de thème est lourd et pas envisageable)

---
+++ Application/src/components/molecules/TabScreenTemplate.tsx
@@ -0,0 +1,9 @@
+import { TabScreen } from 'react-native-paper-tabs';
+import { TabScreenProps } from 'react-native-paper-tabs/src/TabScreen'
+
+export default function TabScreenTemplate(props : Readonly<TabScreenProps>) {
+    return (
+        <TabScreen {...props}>
+        </TabScreen>
+    );
+}
