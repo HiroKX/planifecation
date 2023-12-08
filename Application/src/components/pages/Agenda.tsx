b9a500a11dde69b0834fbba1a205b11b0f439f34
Tim Branstett <tim.branstett@sogeti.com>
2023-12-08 17:01:07 +0100

    Réimplémentation react-native-calendars
    Retour de react-native-calendars
    
    Implémentation de la vue du calendrier et de l'agenda
    
    override de l'event de connexion pour aller plus vite sur le bon composant, ne pas oublier à la fin de rechanger ça

--- Application/src/components/pages/Agenda.tsx
+++ Application/src/components/pages/Agenda.tsx
@@ -3,24 +3,59 @@ import TabScreenTemplate from '../molecules/TabScreenTemplate';
 import TabsTemplate from '../organisms/TabsTemplate';
 import { useState } from 'react';
 import CalendarTemplate from '../organisms/CalendarTemplate';
+import TimelineTemplate from '../organisms/TimelineTemplate';
+import {groupBy} from 'lodash';
+import {
+    TimelineEventProps,
+    CalendarUtils,
+    CalendarProvider
+  } from 'react-native-calendars';
 
 export default function Agenda() {
 
-    const [selected, setSelected] = useState('');
+    const today =  new Date();
+    const getDate = CalendarUtils.getCalendarDateString(new Date().setDate(today.getDate()));
+    const INITIAL_TIME = {hour : today.getHours(), minutes : today.getMinutes()};
+
+    const events: TimelineEventProps[] = [
+        {
+            start : `${getDate} 01:15:00`,
+            end : `${getDate} 12:00:00`,
+            title : 'Test',
+            summary : 'Test d\'un évènement'
+        }, {
+            start : `${getDate} 01:15:00`,
+            end : `${getDate} 12:00:00`,
+            title : 'Test',
+            summary : 'Test d\'un évènement'
+        }
+    ];
+
+    const eventsByDate = groupBy(events, e => CalendarUtils.getCalendarDateString(e.start)) as { [key: string]: TimelineEventProps[];}
+
+    const [selectDay, setSelectDay] = useState(true); // will be used for disabling the day view if no day is selected
+    const [selectEvent, setSelectEvent] = useState(true); // will be used for disabling the details view if no appointment is selected
     
     return (
+        <CalendarProvider
+        date={'now'}
+        >
         <TabsTemplate>
             <TabScreenTemplate label='Mois' icon='calendar'>
                 <View>
                     <CalendarTemplate/>
                 </View>
             </TabScreenTemplate>
-            <TabScreenTemplate label='Jour' icon='view-day'>
-                <View></View>
+            <TabScreenTemplate label='Jour' icon='view-day' disabled={false}>
+                <TimelineTemplate
+                    events={eventsByDate}
+                    initialTime={INITIAL_TIME}
+                    />
             </TabScreenTemplate>
-            <TabScreenTemplate label='Details' icon='account-details' disabled={true}>
+            <TabScreenTemplate label='Details' icon='account-details' disabled={selectEvent}>
             <View></View>
                 </TabScreenTemplate>
-        </TabsTemplate>
+            </TabsTemplate>
+            </CalendarProvider>
     );
   }
