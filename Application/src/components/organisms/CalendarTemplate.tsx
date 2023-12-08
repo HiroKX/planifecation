3591e03e5c1738d7ad6c3fc628dda2722d0fb1d4
Tim Branstett <tim.branstett@sogeti.com>
2023-12-08 17:01:07 +0100

    Réimplémentation react-native-calendars
    Retour de react-native-calendars
    
    Implémentation de la vue du calendrier et de l'agenda
    
    override de l'event de connexion pour aller plus vite sur le bon composant, ne pas oublier à la fin de rechanger ça

--- Application/src/components/organisms/CalendarTemplate.tsx
+++ Application/src/components/organisms/CalendarTemplate.tsx
@@ -1,6 +1,6 @@
 import { Calendar, CalendarProps as Props } from "react-native-calendars";
 
-export default function CalendarTemplate(props : Props) {
+export default function CalendarTemplate(props : Readonly<Props>) {
     return (
         <Calendar
         {...props}/>
