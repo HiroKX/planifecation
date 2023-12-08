aef7ae3c88ca93f96afcda0e77b9cd233a4cacf3
Tim Branstett <tim.branstett@sogeti.com>
2023-12-08 17:01:07 +0100

    Réimplémentation react-native-calendars
    Retour de react-native-calendars
    
    Implémentation de la vue du calendrier et de l'agenda
    
    override de l'event de connexion pour aller plus vite sur le bon composant, ne pas oublier à la fin de rechanger ça

--- Application/src/components/organisms/TabsTemplate.tsx
+++ Application/src/components/organisms/TabsTemplate.tsx
@@ -5,11 +5,14 @@ import {
 import { TabsProviderProps } from 'react-native-paper-tabs/src/utils'
 import { theme } from './OwnPaperProvider';
 
-export default function TabsTemplate(props : Readonly<TabsProviderProps>) {
+export default function TabsTemplate(props: Readonly<TabsProviderProps>) {
     return (
             <TabsProvider
+              
+            {...props}
             defaultIndex={0}>
               <Tabs
+              disableSwipe={true}
               style={{backgroundColor: theme.colors.elevation.level5}}
               iconPosition='top'
               {...props}></Tabs>
