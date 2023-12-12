7df982db9f70ecb7e64a8b6349da3c2bec985387
jakmoino <116438761+jakmoino@users.noreply.github.com>
2023-12-12 23:22:12 +0100

    construction page commencée

--- Application/src/components/pages/NoteList.tsx
+++ Application/src/components/pages/NoteList.tsx
@@ -1,3 +1,11 @@
 import { StyleSheet, View } from 'react-native';
 import { ReactNode, useState } from 'react';
 import TextInputTemplate from '../atoms/styles/TextInputTemplate';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { StackParamList } from '../../navigation/RootStack';
+
+type Props = NativeStackScreenProps<StackParamList>;
+
+export default function NoteList(props : Props): ReactNode{
+    return (true);
+}
