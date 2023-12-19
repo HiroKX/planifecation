69b6dc8c192586b546b7816ed3b1dbb809b00c9d
Tim <tim.branstett@gmail.com>
2023-12-19 18:08:19 +0100

    SplashScreen opérationnel
    Des tests avec Animated ont été effectué mais c'est une fonctionnalité mal faite. Bug dans la doc officielle de React avec comme fix proposé : une librairie pas mise à jour depuis 2 ans.
    
    Animations laissées de côté pour le moment

--- Application/src/components/atoms/styles/LogoTemplate.tsx
+++ Application/src/components/atoms/styles/LogoTemplate.tsx
@@ -3,13 +3,14 @@ import { Image } from 'react-native';
 type Props = {
     width? : number,
     height? : number
+    style? : StyleProp<ImageStyle>
 }
 
 export default function LogoTemplate(props? : Readonly<Props> ) {
     return (
-        <Image width={props?.width ?? 250} height={props?.height ?? 250}
+        <Image width={props?.style?.width ?? 250} height={props?.style?.height ?? 250}
             resizeMode='stretch'
-            style={{height: props?.height, width : props?.width}}
+            style={props?.style}
             source={require('../../../assets/logo.png')}/>
     );
 }
