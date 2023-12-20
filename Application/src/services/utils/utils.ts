1b15e7dc35d1d956ffee48091d0a35d6ef36ef77
Tim <tim.branstett@gmail.com>
2023-12-20 20:02:44 +0100

    optimisation locale + utils pour luxon plus facile

--- Application/src/services/utils/utils.ts
+++ Application/src/services/utils/utils.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon';
 import { locale } from '../../environment/locale';
-import { DateData, LocaleConfig } from 'react-native-calendars';
+import { DateData } from 'react-native-calendars';
+import { todayString } from 'react-native-calendars/src/expandableCalendar/commons';
 
 // Return the perfect matching color for text based on hexcolor background
 // It uses the YIQ model to calculate the best match
@@ -52,4 +53,3 @@ export class LuxonDate {
       .toFormat('MMMM yyyy');
   }
 }
-LocaleConfig.defaultLocale = 'fr'; // loads french equivalent of label in calendar and timeline
