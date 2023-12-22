7cd769caa1a3dc1025c9ebd1fb32db603a2010bd
Tim <tim.branstett@gmail.com>
2023-12-22 15:03:52 +0100

    Implémentation redux dans la vue
    Essaie de l'implémentation de redux dans la vue avant de l'abandonner au profit des signaux. Mais je voulais au moins comprendre comment ça marchait et tester les useSelector / useDispatch
    
    Suppression de locale.ts pour tout mettre dans utils.ts

--- Application/src/components/pages/Agenda/CalendarTemplate.tsx
+++ Application/src/components/pages/Agenda/CalendarTemplate.tsx
@@ -1,13 +1,14 @@
 import { Calendar } from "react-native-calendars";
 import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../../../store/store";
+import { RootState, setSelectDate } from "../../../store/store";
 
 
 export default function CalendarTemplate() {
-    const selectDate = useSelector((state : RootState) => state.selectDate.date.dateString);
-
+    const selectDate = useSelector((state : RootState) => state.selectDate);
+    const dispatch = useDispatch();
     return(
         <Calendar
+            onDayPress={(date) => dispatch(setSelectDate(date))}
             />
     )
 
