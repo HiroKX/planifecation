624cd13e0af3d6cfd867d7f612f6463f7ce6eed3
Tim Branstett <tim.branstett@gmail.com>
2024-01-04 17:25:27 +0100

    Premiers essais GestureHandler + SKIA

---
+++ Application/src/components/pages/TestSKIA.tsx
@@ -0,0 +1,218 @@
+import {
+  Canvas,
+  Path,
+  SkPath,
+  Skia,
+  TouchInfo,
+  useTouchHandler,
+} from "@shopify/react-native-skia";
+import React, { useCallback, useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+type PathWithColorAndWidth = {
+  path: SkPath;
+  color: Color;
+  strokeWidth: number;
+};
+
+export default function TestSkia() {
+  const [paths, setPaths] = useState<PathWithColorAndWidth[]>([]);
+  const [color, setColor] = useState<Color>(Colors[0]);
+
+  const [strokeWidth, setStrokeWidth] = useState(strokes[0]);
+
+  const onDrawingStart = useCallback(
+    (touchInfo: TouchInfo) => {
+      setPaths((currentPaths) => {
+        const { x, y } = touchInfo;
+        const newPath = Skia.Path.Make();
+        newPath.moveTo(x, y);
+        return [
+          ...currentPaths,
+          {
+            path: newPath,
+            color,
+            strokeWidth,
+          },
+        ];
+      });
+    },
+    [color, strokeWidth]
+  );
+
+  const onDrawingActive = useCallback((touchInfo: TouchInfo) => {
+    setPaths((currentPaths) => {
+      const { x, y } = touchInfo;
+      const currentPath = currentPaths[currentPaths.length - 1];
+      const lastPoint = currentPath.path.getLastPt();
+      const xMid = (lastPoint.x + x) / 2;
+      const yMid = (lastPoint.y + y) / 2;
+
+      currentPath.path.quadTo(lastPoint.x, lastPoint.y, xMid, yMid);
+      return [...currentPaths.slice(0, currentPaths.length - 1), currentPath];
+    });
+  }, []);
+
+  const touchHandler = useTouchHandler(
+    {
+      onActive: onDrawingActive,
+      onStart: onDrawingStart,
+    },
+    [onDrawingActive, onDrawingStart]
+  );
+
+  return (
+    <View style={style.container}>
+      <Toolbar
+        color={color}
+        strokeWidth={strokeWidth}
+        setColor={setColor}
+        setStrokeWidth={setStrokeWidth}
+      />
+      <Canvas style={style.container} onTouch={touchHandler}>
+        {paths.map((path, index) => (
+          <Path
+            key={index}
+            path={path.path}
+            color={path.color}
+            style={"stroke"}
+            strokeWidth={path.strokeWidth}
+          />
+        ))}
+      </Canvas>
+    </View>
+  );
+};
+
+const Colors = ["black", "red", "blue", "green", "yellow", "white"] as const;
+
+type Color = (typeof Colors)[number];
+
+type ToolbarProps = {
+  color: Color;
+  strokeWidth: number;
+  setColor: (color: Color) => void;
+  setStrokeWidth: (strokeWidth: number) => void;
+};
+
+const strokes = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+
+const Toolbar = ({
+  color,
+  strokeWidth,
+  setColor,
+  setStrokeWidth,
+}: ToolbarProps) => {
+  const [showStrokes, setShowStrokes] = useState(false);
+
+  const handleStrokeWidthChange = (stroke: number) => {
+    setStrokeWidth(stroke);
+    setShowStrokes(false);
+  };
+
+  const handleChangeColor = (color: Color) => {
+    setColor(color);
+  };
+
+  return (
+    <>
+      {showStrokes && (
+        <View style={[style.toolbar, style.strokeToolbar]}>
+          {strokes.map((stroke) => (
+            <Pressable
+              onPress={() => handleStrokeWidthChange(stroke)}
+              key={stroke}
+            >
+              <Text style={style.strokeOption}>{stroke}</Text>
+            </Pressable>
+          ))}
+        </View>
+      )}
+      <View style={[style.toolbar]}>
+        <Pressable
+          style={style.currentStroke}
+          onPress={() => setShowStrokes(!showStrokes)}
+        >
+          <Text>{strokeWidth}</Text>
+        </Pressable>
+        <View style={style.separator} />
+        {Colors.map((item) => (
+          <ColorButton
+            isSelected={item === color}
+            key={item}
+            color={item}
+            onPress={() => handleChangeColor(item)}
+          />
+        ))}
+      </View>
+    </>
+  );
+};
+
+type ColorButtonProps = {
+  color: Color;
+  isSelected: boolean;
+  onPress: () => void;
+};
+
+const ColorButton = ({ color, onPress, isSelected }: ColorButtonProps) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[
+        style.colorButton,
+        { backgroundColor: color },
+        isSelected && {
+          borderWidth: 2,
+          borderColor: "black",
+        },
+      ]}
+    />
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+  },
+  strokeOption: {
+    fontSize: 18,
+    backgroundColor: "#f7f7f7",
+  },
+  toolbar: {
+    backgroundColor: "#ffffff",
+    height: 50,
+    width: 300,
+    borderRadius: 100,
+    borderColor: "#f0f0f0",
+    borderWidth: 1,
+    flexDirection: "row",
+    paddingHorizontal: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+  separator: {
+    height: 30,
+    borderWidth: 1,
+    borderColor: "#f0f0f0",
+    marginHorizontal: 10,
+  },
+  currentStroke: {
+    backgroundColor: "#f7f7f7",
+    borderRadius: 5,
+  },
+  strokeToolbar: {
+    position: "absolute",
+    top: 70,
+    justifyContent: "space-between",
+    zIndex: 100,
+  },
+  colorButton: {
+    width: 30,
+    height: 30,
+    borderRadius: 100,
+    marginHorizontal: 5,
+  },
+});
