7a928d1e6b59b1b3d5cfb0009bfed574d366c472
robin <57806687+HiroKX@users.noreply.github.com>
2024-01-05 12:45:51 +0100

    Fix : GraphQL

---
+++ Serveur/dist/queries/NoteQueries.js
@@ -0,0 +1,34 @@
+import prisma from '../prismaClient.js';
+import { exclude, protectFromUsername } from '../tools/tools.js';
+export const noteQueries = {
+    getNoteById: async (parent, args, context) => {
+        const note = await prisma.note.findFirstOrThrow({
+            where: {
+                id: args.id
+            },
+            include: {
+                user: true,
+            }
+        });
+        protectFromUsername(context, note.user.username);
+        const userWithoutPassword = exclude(note.user, ['password']);
+        return { ...note, user: userWithoutPassword };
+    },
+    getAllNotesByUsername: async (parent, args, context) => {
+        protectFromUsername(context, args.username);
+        const notes = await prisma.note.findMany({
+            where: {
+                user: {
+                    username: args.username
+                }
+            },
+            include: {
+                user: true,
+            },
+        });
+        return notes.map(note => {
+            const userWithoutPassword = exclude(note.user, ['password']);
+            return { ...note, user: userWithoutPassword };
+        });
+    },
+};
