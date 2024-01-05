a2686e1d04fbf094c42d28f1fd7e5894f13b24b4
robin <57806687+HiroKX@users.noreply.github.com>
2024-01-05 12:45:51 +0100

    Fix : GraphQL

---
+++ Serveur/src/prismaClient.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (!global.prisma) {
+    global.prisma = new PrismaClient();
+}
+
+prisma = global.prisma;
+
+export default prisma;
