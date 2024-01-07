617392a3162c1026ae62d68b702c42ecf73ed7b6
Tim <tim.branstett@gmail.com>
2024-01-07 17:02:39 +0100

    Draw fonctionnel
    Fonctionnel.
    
    A revoir : Calcul de la surface coloriée / Dépassement de la fenêtre en dessinant / Design

--- Application/src/components/pages/Todo/CheckTodo.tsx
+++ Application/src/components/pages/Todo/CheckTodo.tsx
@@ -1,14 +1,19 @@
 import { Canvas, useImage, Image, SkPath, Skia, Mask, Group, Rect, Path } from "@shopify/react-native-skia";
-import { GestureDetector, Gesture } from "react-native-gesture-handler";
+import { GestureDetector, Gesture, GestureHandlerRootView } from "react-native-gesture-handler";
 import { View, LayoutChangeEvent } from "react-native";
 import { useCallback, useMemo, useRef, useState } from 'react';
 import { svgPathProperties } from "svg-path-properties";
+import { Todo } from "../../../models/Todo";
 
 const STROKE_WIDTH = 10;
 
 interface LayerProps  {
     width : number,
     height : number
+}
+
+interface TodoProps {
+    returnFunc : (p : Todo) => void;
 }
 
 
@@ -30,7 +35,7 @@ const CoveredView = ({width, height} : LayerProps) => {
     )
 }
 
-export default function CheckTodo() {
+export default function CheckTodo( props : Readonly<TodoProps>) {
 
 
     const [canvasLayoutMeta, setCanvasLayoutMeta] = useState({
@@ -38,7 +43,6 @@ export default function CheckTodo() {
         height : 0,
     });
     const totalArea= useRef<number>(0);
-    const [treshold, setTreshold] = useState(false);
     const [paths, setPaths] = useState<SkPath[]>([])
     const handleLayoutCanvas = useCallback((e : LayoutChangeEvent) => {
         const {width, height} = e.nativeEvent.layout;
@@ -70,46 +74,47 @@ export default function CheckTodo() {
         const areaCheckedPercentage = (totalArea.current / (width*height)) * 100;
         console.log("areaCheckedPercentage : " +areaCheckedPercentage);
         if(areaCheckedPercentage > 80) {
-            alert("Treshold atteint ✔");
+            props.returnFunc(props.item);
         }
     })
     .minDistance(1)
-    .enabled(!treshold)
 
     const {width, height} = useMemo(() => canvasLayoutMeta, [canvasLayoutMeta]);
 
     return (
         <View style={{ width:200, height: 200, backgroundColor: 'blue'}}>
-            <GestureDetector gesture={checkTodoPan}>
-                    <Canvas onLayout={handleLayoutCanvas} style={{width: '100%', height: '100%'}}>
-                        <CheckedView width={width} height={height} />
-                        <Mask clip mode="luminance" mask={<Group>
-                            <Rect
-                            x={0}
-                            y={0}
-                            width={width}
-                            height={height}
-                            color='white'
-                            />
-                            {
-                                paths.map(p => (
-                                    <Path
-                                        key={p.toSVGString()}
-                                        path={p}
-                                        strokeWidth={STROKE_WIDTH}
-                                        style="stroke"
-                                        strokeJoin="round"
-                                        strokeCap="round"
-                                        antiAlias
-                                        color='black' />
-                                ))
-                            }
-                        </Group>
-                        }>
-                            <CoveredView width={width} height={height} />
-                        </Mask>
-                    </Canvas>
-            </GestureDetector>
+            <GestureHandlerRootView>
+                <GestureDetector gesture={checkTodoPan}>
+                        <Canvas onLayout={handleLayoutCanvas} style={{width: '100%', height: '100%'}}>
+                            <CheckedView width={width} height={height} />
+                            <Mask clip mode="luminance" mask={<Group>
+                                <Rect
+                                x={0}
+                                y={0}
+                                width={width}
+                                height={height}
+                                color='white'
+                                />
+                                {
+                                    paths.map(p => (
+                                        <Path
+                                            key={p.toSVGString()}
+                                            path={p}
+                                            strokeWidth={STROKE_WIDTH}
+                                            style="stroke"
+                                            strokeJoin="round"
+                                            strokeCap="round"
+                                            antiAlias
+                                            color='black' />
+                                    ))
+                                }
+                            </Group>
+                            }>
+                                <CoveredView width={width} height={height} />
+                            </Mask>
+                        </Canvas>
+                </GestureDetector>
+            </GestureHandlerRootView>
         </View>
     );
 }
