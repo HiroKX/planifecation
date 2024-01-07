6d683f7ac3ba6c231af33fd74f40564342449e27
Tim <tim.branstett@gmail.com>
2024-01-07 18:20:30 +0100

    update checkbox iOS (debug temporaire)

--- Application/src/components/molecules/CheckboxTemplate.tsx
+++ Application/src/components/molecules/CheckboxTemplate.tsx
@@ -1,13 +1,15 @@
+import { View } from 'react-native';
 import { Checkbox, CheckboxProps } from 'react-native-paper';
-
-interface Todo {
-  id: string;
-  content: string;
-  isDone?: boolean;
-}
+import { theme } from '../organisms/OwnPaperProvider';
 
 export default function CheckboxTemplate(
   props: Readonly<CheckboxProps>
 ): React.ReactNode {
-  return <Checkbox onPress={props.onPress} {...props}></Checkbox>;
+  return (
+  <View
+  style={{borderWidth: 1 }}>
+    <Checkbox 
+    onPress={props.onPress} {...props}></Checkbox>
+  </View>
+  );
 }
