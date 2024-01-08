b4d3504ae8d7f82acea3c8a83720430781109c7c
Tim <tim.branstett@gmail.com>
2024-01-08 14:48:37 +0100

    Prettier

--- Application/src/components/molecules/CheckboxTemplate.tsx
+++ Application/src/components/molecules/CheckboxTemplate.tsx
@@ -1,18 +1,22 @@
 import { IconButton, IconButtonProps } from 'react-native-paper';
 
 interface IconOverrideProps {
-  iconButton : IconButtonProps;
-  status : 'checked' | 'unchecked';
-  onPress : () => void;
+  iconButton: IconButtonProps;
+  status: 'checked' | 'unchecked';
+  onPress: () => void;
 }
-
 
 export default function CheckboxTemplate(
   props: Readonly<IconOverrideProps>
 ): React.ReactNode {
   return (
-      <IconButton
-      {...props} 
-      icon={props.status === 'unchecked' ? 'checkbox-blank-outline' : 'checkbox-marked' }/>
+    <IconButton
+      {...props}
+      icon={
+        props.status === 'unchecked'
+          ? 'checkbox-blank-outline'
+          : 'checkbox-marked'
+      }
+    />
   );
 }
