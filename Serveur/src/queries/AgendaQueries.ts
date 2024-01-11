75a9dceaf7003115947b001344e92d76c78c3484
unknown <thomas.felker8@etu.univ-lorraine.fr>
2024-01-11 16:28:07 +0100

    Ajout des requêtes liées à l'agenda

---
+++ Serveur/src/queries/AgendaQueries.ts
@@ -0,0 +1,38 @@
+import prisma from "../prismaClient.js";
+import { exclude, protectFromUsername } from "../tools/tools.js";
+
+export const agendaQueries = {
+    getAgendaEventById: async (parent, args, context) => {
+        // Get an Agenda Event in the db by its id
+        const agendaEvent = await prisma.AgendaEvent.findFirstOrThrow({
+            where: {
+                id: args.id,
+            },
+            include: {
+                user: true,
+            },
+        });
+        protectFromUsername(context, AgendaEvent.user.username);
+        const userWithoutPassword = exclude(agendaEvent.user, ["password"]);
+        return { ...agendaEvent, user: userWithoutPassword };
+    },
+
+    getAllTodoItemsByUsername: async (parent, args, context) => {
+        // Get all the Agenda Events by the username of the User who owns the events
+        protectFromUsername(context, args.username);
+        const agendaEvents = await prisma.AgendaEvent.findMany({
+            where: {
+                user: {
+                    username: args.username,
+                },
+            },
+            include: {
+                user: true,
+            },
+        });
+        return agendaEvents.map((event) => {
+            const userWithoutPassword = exclude(event.user, ["password"]);
+            return { ...event, user: userWithoutPassword };
+        });
+    },
+};
