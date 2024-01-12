82d8881319f6368bfe2a47f237609ca077d14634
Tim <tim.branstett@gmail.com>
2024-01-12 15:05:16 +0100

    Implémentation signaux
    Premier test concluant, comportement à copier

--- Application/src/components/pages/Agenda/Appointments.tsx
+++ Application/src/components/pages/Agenda/Appointments.tsx
@@ -1,21 +1,21 @@
-import { Text } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { LuxonDate, todayData } from "../../../services/utils/utils";
-import CalendarTemplate from "./CalendarTemplate";
+import { Text } from "react-native-paper";
 import AgendaTemplate from "./AgendaTemplate";
-import { computed, signal } from "@preact/signals-react";
+import CalendarTemplate from "./CalendarTemplate";
+import { signal, useSignal } from "@preact/signals-react";
 
 const Tab = createMaterialTopTabNavigator();
-export const selectDate = signal(todayData.dateString);
 
+export const simple = signal("Test simple");
 
 export default function Appointments() {
+
+    const displaySimple = useSignal(simple);
+
     const firstTabLabel = () => {
-        return (<Text> {LuxonDate.to_MMMMyyyy(selectDate.value).toUpperCase()}</Text>);
+        return <Text>{displaySimple.value}</Text>
     }
-    const secondTabLabel = () => {
-        return (<Text> {LuxonDate.to_jourdd(selectDate.value)} </Text>)
-    }
+
     return (
             <Tab.Navigator
                 initialRouteName="Calendrier">
@@ -23,7 +23,7 @@ export default function Appointments() {
                         options={{tabBarLabel : firstTabLabel}}/>
                     
                     <Tab.Screen name="Agendeux" component={AgendaTemplate}
-                        options={{tabBarLabel : secondTabLabel}}/>
+                        options={{tabBarLabel : "Non"}}/>
             </Tab.Navigator>
     );
 
