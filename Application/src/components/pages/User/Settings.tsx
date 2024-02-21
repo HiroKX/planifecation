808b8573cab2365325b3177b5b438d32a6ee25b2
Tim Branstett <tim.branstett@gmail.com>
2024-02-21 14:51:13 +0100

    Revert "test slider"
    This reverts commit fb3c6e5dd4b6b4084a55b297badcbeda165f365f.

--- Application/src/components/pages/User/Settings.tsx
+++ Application/src/components/pages/User/Settings.tsx
@@ -2,32 +2,21 @@ import {
   Divider,
   Switch as PaperSwitch,
   Text as PaperText,
-  Text,
 } from 'react-native-paper';
 import ButtonTemplate from '../../atoms/styles/ButtonTemplate';
 import SurfaceTemplate from '../../molecules/SurfaceTemplate';
-import { Children, ReactNode, useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { LogoutUser } from '../../../controllers/AuthenticationController';
 import { StackParamList } from '../../../navigation/RootStack';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { DeleteAndLogoutUser } from '../../../controllers/UserController';
 import { ApolloConsumer } from '@apollo/client';
-import { useSharedValue } from 'react-native-reanimated';
-import SliderTemplate from '../../atoms/styles/SliderTemplate';
-import TextTemplate from '../../atoms/styles/TextTemplate';
-import TextInputTemplate from '../../atoms/styles/TextInputTemplate';
 
 type Props = NativeStackScreenProps<StackParamList>;
 
 function Settings(props: Readonly<Props>): ReactNode {
   const [themeSlideEnabled, setThemeSlideEnabled] = useState(false);
   const toggleThemeSwitch = () => setThemeSlideEnabled(!themeSlideEnabled);
-  const progressSlider = useSharedValue(0);
-  const minValueSlider = useSharedValue(0);
-  const maxValueSlider = useSharedValue(2000);
-
-  const [boosterLevel, setBoosterLevel] = useState("Sans boost")
-
   return (
     <ApolloConsumer>
       {client => (
@@ -70,31 +59,6 @@ function Settings(props: Readonly<Props>): ReactNode {
           <ButtonTemplate onPress={() => {}}>
             Accéder aux conditions générales
           </ButtonTemplate>
-          <SliderTemplate 
-          progress={progressSlider} 
-          minimumValue={minValueSlider} 
-          maximumValue={maxValueSlider}
-          step={4}
-          onValueChange={ (progress) => {
-            switch(progress) {
-              case 500 :
-                setBoosterLevel("Fast !");
-                break;
-              case 1000 :  
-                setBoosterLevel("Super fast !!");
-                break;
-              case 1500 :  
-                setBoosterLevel("Hyper fast !!!");
-                break;
-              case 2000 : 
-                setBoosterLevel("ULTRA FAST !!!!!");
-                break;
-              default:
-                setBoosterLevel("Sans boost");
-            }
-          }}
-          ></SliderTemplate>
-          <TextTemplate>{boosterLevel}</TextTemplate>
         </SurfaceTemplate>
       )}
     </ApolloConsumer>
