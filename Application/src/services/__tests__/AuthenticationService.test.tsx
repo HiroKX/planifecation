a33e84c0b498aed7a224f23d0edf0be36df5c963
Tim Branstett <tim.branstett@gmail.com>
2024-02-21 16:28:00 +0100

    Revert "PRETTIER"
    This reverts commit 05c6c8390119767f3344b89623803e6bddb785b2.

--- Application/src/services/__tests__/AuthenticationService.test.tsx
+++ Application/src/services/__tests__/AuthenticationService.test.tsx
@@ -1,4 +1,8 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from '@apollo/client';
 import { CreateUser, LogUser } from '../AuthenticationService';
 import fetch from 'cross-fetch';
 import { URI_API } from '@env';
@@ -20,8 +24,8 @@ describe('User Test', () => {
 
   const username = 'aABaaaA';
   let password = 'aABaaaA';
-  let result: number;
-  let token: string;
+  let result : number;
+  let token : string;
 
   it('should successfully create a user', async () => {
     result = await CreateUser(client, username, password);
@@ -29,15 +33,16 @@ describe('User Test', () => {
   });
 
   it('should fail to create a user', async () => {
-    const response = await CreateUser(client, username, password);
-    expect(response).toThrow();
-  });
+      const response = await CreateUser(client, username, password);
+      expect(response).toThrow();
+    });
 
   it('Should successfully log the user', async () => {
     const response = await LogUser(client, username, password);
-    expect(response).toContain('logUser');
-    const fromJSON = JSON.parse(response);
+    expect(response).toContain("logUser");
+    const fromJSON = JSON.parse(response)
     token = fromJSON.logUser;
     expect(token).not.toBe(null);
+  } )
+
   });
-});
