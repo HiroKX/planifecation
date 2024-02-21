2782e43aaa593a2abfb931c73b3eb80403a88aaf
jakmoino <116438761+jakmoino@users.noreply.github.com>
2024-02-21 21:42:36 +0100

    ajout usesharedvalue

--- Application/src/components/atoms/styles/SliderTemplate.tsx
+++ Application/src/components/atoms/styles/SliderTemplate.tsx
@@ -1,18 +1,21 @@
 import { ReactNode } from 'react';
 import { Slider } from 'react-native-awesome-slider';
+import { useSharedValue } from 'react-native-reanimated';
 import { SliderProps } from 'reanimated-color-picker';
 
 export default function ButtonTemplate(
     props: Readonly<SliderProps>
   ): ReactNode {
+
+    const progress = useSharedValue(30);
+    const min = useSharedValue(0);
+    const max = useSharedValue(100);
+  
     return (
-      <Slider progress={{
-            value: 0
-        }} minimumValue={{
-            value: 0
-        }} maximumValue={{
-            value: 0
-        }}        
+      <Slider 
+       progress={progress}
+       minimumValue={min} 
+       maximumValue={max}        
       />
     );
   }
