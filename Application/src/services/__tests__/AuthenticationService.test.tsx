05c6c8390119767f3344b89623803e6bddb785b2
Tim Branstett <tim.branstett@gmail.com>
2024-02-21 15:34:37 +0100

    PRETTIER

--- Application/src/services/__tests__/AuthenticationService.test.tsx
+++ Application/src/services/__tests__/AuthenticationService.test.tsx
@@ -1,8 +1,4 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-} from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import { CreateUser, LogUser } from '../AuthenticationService';
 import fetch from 'cross-fetch';
 import { URI_API } from '@env';
@@ -24,8 +20,8 @@ describe('User Test', () => {
 
   const username = 'aABaaaA';
   let password = 'aABaaaA';
-  let result : number;
-  let token : string;
+  let result: number;
+  let token: string;
 
   it('should successfully create a user', async () => {
     result = await CreateUser(client, username, password);
@@ -33,16 +29,15 @@ describe('User Test', () => {
   });
 
   it('should fail to create a user', async () => {
-      const response = await CreateUser(client, username, password);
-      expect(response).toThrow();
-    });
+    const response = await CreateUser(client, username, password);
+    expect(response).toThrow();
+  });
 
   it('Should successfully log the user', async () => {
     const response = await LogUser(client, username, password);
-    expect(response).toContain("logUser");
-    const fromJSON = JSON.parse(response)
+    expect(response).toContain('logUser');
+    const fromJSON = JSON.parse(response);
     token = fromJSON.logUser;
     expect(token).not.toBe(null);
-  } )
-
   });
+});
