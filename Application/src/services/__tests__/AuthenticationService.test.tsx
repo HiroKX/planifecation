103d230e0b57dc8c48da7dc601398fed3839f6c7
Tim Branstett <tim.branstett@gmail.com>
2024-02-21 16:28:08 +0100

    Revert "update URL absolue"
    This reverts commit bf2141f5850a534de20579ed42cd82a68580684c.

--- Application/src/services/__tests__/AuthenticationService.test.tsx
+++ Application/src/services/__tests__/AuthenticationService.test.tsx
@@ -13,9 +13,9 @@ describe('User Test', () => {
   beforeEach(() => {
     client = new ApolloClient({
       cache: new InMemoryCache(),
-      uri: `${URI_API}`,
+      uri: URI_API,
       link: new HttpLink({
-        uri: `${URI_API}`,
+        uri: URI_API,
         fetch,
       }),
       connectToDevTools: false, // Disable DevTools in test environment
