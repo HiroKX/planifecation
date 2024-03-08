3be7d968e88892177a7219aeb151f62728565f4c
Tim <tim.branstett@gmail.com>
2024-03-08 12:56:47 +0100

    update event et prettier

--- Application/src/components/pages/Dashboard.tsx
+++ Application/src/components/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import SurfaceTemplate from '../molecules/SurfaceTemplate';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import AppTemplate from '../atoms/AppTemplate';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StackParamList } from '../../navigation/RootStack';
@@ -31,55 +31,63 @@ export default function Dashboard(props: Readonly<Props>): ReactNode {
       {client => (
         <View style={styles.mainContainer}>
           <SurfaceTemplate style={styles.surfaces}>
-            <AppTemplate
-              customSize={80}
-              icon="note-outline"
-              elevation={5}
+            <TouchableOpacity
+              style={styles.rowflex}
               onPress={() => props.navigation.navigate('Liste des notes')}
-            />
-            <TextTemplate variant="headlineLarge" style={styles.text}>
-              Bloc-notes
-            </TextTemplate>
+            >
+              <AppTemplate customSize={80} icon="note-outline" elevation={5} />
+              <TextTemplate variant="headlineLarge" style={styles.text}>
+                Bloc-notes
+              </TextTemplate>
+            </TouchableOpacity>
           </SurfaceTemplate>
           <SurfaceTemplate style={[styles.surfaces, styles.endflex]}>
-            <TextTemplate variant="headlineLarge" style={styles.text}>
-              Agenda
-            </TextTemplate>
-            <AppTemplate
-              customSize={80}
-              icon="calendar-cursor"
-              elevation={5}
+            <TouchableOpacity
+              style={[styles.rowflex, styles.endflex]}
               onPress={async () => {
                 console.debug('Retrieving events');
                 const events = await getAgendaEvents(client);
                 console.debug('Events retrieved');
                 props.navigation.navigate('Agenda', { events });
               }}
-            />
+            >
+              <TextTemplate variant="headlineLarge" style={styles.text}>
+                Agenda
+              </TextTemplate>
+              <AppTemplate
+                customSize={80}
+                icon="calendar-cursor"
+                elevation={5}
+              />
+            </TouchableOpacity>
           </SurfaceTemplate>
           <SurfaceTemplate style={styles.surfaces}>
-            <AppTemplate
-              customSize={80}
-              icon="format-list-checkbox"
-              elevation={5}
+            <TouchableOpacity
+              style={styles.rowflex}
               onPress={() => props.navigation.navigate('Liste toute douce')}
-            />
-            <TextTemplate variant="headlineLarge" style={styles.text}>
-              Liste toute douce
-            </TextTemplate>
+            >
+              <AppTemplate
+                customSize={80}
+                icon="format-list-checkbox"
+                elevation={5}
+              />
+              <TextTemplate variant="headlineLarge" style={styles.text}>
+                Liste toute douce
+              </TextTemplate>
+            </TouchableOpacity>
           </SurfaceTemplate>
           <SurfaceTemplate style={[styles.surfaces, styles.endflex]}>
-            <TextTemplate variant="headlineLarge" style={styles.text}>
-              Déconnexion
-            </TextTemplate>
-            <AppTemplate
-              customSize={80}
-              icon="door-sliding"
-              elevation={5}
+            <TouchableOpacity
+              style={[styles.rowflex, styles.endflex]}
               onPress={async () => {
                 await LogoutUser(client, props);
               }}
-            />
+            >
+              <TextTemplate variant="headlineLarge" style={styles.text}>
+                Déconnexion
+              </TextTemplate>
+              <AppTemplate customSize={80} icon="door-sliding" elevation={5} />
+            </TouchableOpacity>
           </SurfaceTemplate>
         </View>
       )}
@@ -97,8 +105,14 @@ const styles = StyleSheet.create({
     borderRadius: 18,
     flexDirection: 'row',
   },
+  rowflex: {
+    flex: 1,
+    flexDirection: 'row',
+  },
   endflex: {
+    flexDirection: 'row',
     justifyContent: 'flex-end',
+    alignSelf: 'flex-end',
   },
   text: {
     color: theme.colors.tertiary,
